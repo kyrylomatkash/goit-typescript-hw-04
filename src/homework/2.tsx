6c9c0f2e7918d4e39bf0bfa94e2fef757daefcbf
Kyrylo <kyrylo.mm@gmail.com>
2024-02-04 00:03:42 +0100

    Done second part of homework

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,4 +1,19 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from 'react';
+
+type RequestStep = 'start' | 'pending' | 'finished' | 'idle';
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+
+type Action = {
+  type:
+    | 'START_REQUEST'
+    | 'PENDING_REQUEST'
+    | 'FINISH_REQUEST'
+    | 'RESET_REQUEST';
+};
 
 const initialState: State = {
   isRequestInProgress: false,
@@ -21,7 +36,10 @@ function requestReducer(state: State, action: Action): State {
 }
 
 export function RequestComponent() {
-  const [requestState, requestDispatch] = useReducer(requestReducer, initialState);
+  const [requestState, requestDispatch] = useReducer(
+    requestReducer,
+    initialState
+  );
 
   const startRequest = () => {
     requestDispatch({ type: 'START_REQUEST' });
